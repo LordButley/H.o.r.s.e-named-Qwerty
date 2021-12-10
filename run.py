ce1c93e915ef96fe29da44c6306d9b7881869b34
LordButley <joshjwwilson@gmail.com>
2021-12-10 19:14:22 +0000

    add answer sets. add select_theme function opening

--- run.py
+++ run.py
@@ -23,13 +23,24 @@ print("Take a typing challenge and see how you compare to others")
 print("You have 100 seconds to type as many answers as possible")
 print("You can only make so many mistakes")
 print("Good luck!")
-score_data = [0 ,0 ,0]
+score_data = [0, 0, 0]
 username = input("What is your name? ")
 
+ANSWERS_STAR_WARS = {"Jedi", "Lightsaber", "Skywalker", "Luke", "Leia", "Darth", "Vader", "Maul", "Padawan", "Obi wan", "Kenobi", "Millenium", "Falcon", "Clone", "Droid", "Palpatine", "Emporer", "Republic", "Galaxy", "Hoth", "Endor", "Anakin", "Han Solo", "Tatooine"}
+ANSWERS_ELEMENTS = {"Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium"}
+ANSWERS_HARRY_POTTER ={"Harry", "Potter", "Ron", "Weasley", "Hermione", "Granger", "Voldemort", "Snape", "Professor", "Dumbledore", "Draco", "Malfoy", "Wand", "Owl", "Broomstick", "Philosopher", "Chamber", "Prisoner", "Azkaban", "Dementor"}
+
 def select_theme():
+    '''
+    Function to select the theme of the typing challenge.
+    '''
     print("Typing themes:")
     print("1 - Star Wars")
-    print("Harry Potter")
-    print("Beer brands")
-    print("Periodic table of elements")
+    print("2 - Harry Potter")
+    # print("3 - Beer brands")
+    print("3 - Periodic table of elements")
+    theme_selection = input("Please enter the number of your choice: ")
+    if theme_selection == 1:
+        
+
 
