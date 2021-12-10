4bb8fb23c03c27cab5e162b0782ac3b5f2efcd2e
LordButley <joshjwwilson@gmail.com>
2021-12-10 20:30:14 +0000

    convert answer sets to lists. add if loop to select_theme

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -26,9 +27,9 @@ print("Good luck!")
 score_data = [0, 0, 0]
 username = input("What is your name? ")
 
-ANSWERS_STAR_WARS = {"Jedi", "Lightsaber", "Skywalker", "Luke", "Leia", "Darth", "Vader", "Maul", "Padawan", "Obi wan", "Kenobi", "Millenium", "Falcon", "Clone", "Droid", "Palpatine", "Emporer", "Republic", "Galaxy", "Hoth", "Endor", "Anakin", "Han Solo", "Tatooine"}
-ANSWERS_ELEMENTS = {"Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium"}
-ANSWERS_HARRY_POTTER ={"Harry", "Potter", "Ron", "Weasley", "Hermione", "Granger", "Voldemort", "Snape", "Professor", "Dumbledore", "Draco", "Malfoy", "Wand", "Owl", "Broomstick", "Philosopher", "Chamber", "Prisoner", "Azkaban", "Dementor"}
+ANSWERS_STAR_WARS = ["Jedi", "Lightsaber", "Skywalker", "Luke", "Leia", "Darth", "Vader", "Maul", "Padawan", "Obi wan", "Kenobi", "Millenium", "Falcon", "Clone", "Droid", "Palpatine", "Emporer", "Republic", "Galaxy", "Hoth", "Endor", "Anakin", "Han Solo", "Tatooine"]
+ANSWERS_ELEMENTS = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium"]
+ANSWERS_HARRY_POTTER = ["Harry", "Potter", "Ron", "Weasley", "Hermione", "Granger", "Voldemort", "Snape", "Professor", "Dumbledore", "Draco", "Malfoy", "Wand", "Owl", "Broomstick", "Philosopher", "Chamber", "Prisoner", "Azkaban", "Dementor"]
 
 def select_theme():
     '''
@@ -40,7 +41,16 @@ def select_theme():
     # print("3 - Beer brands")
     print("3 - Periodic table of elements")
     theme_selection = input("Please enter the number of your choice: ")
-    if theme_selection == 1:
+    if theme_selection == "1":
+        answers = random.sample(ANSWERS_STAR_WARS, 10)
+        print(answers)
+    elif theme_selection == "2":
+        answers = random.sample(ANSWERS_HARRY_POTTER, 10)
+        print(answers)
+    elif theme_selection == "3":
+        answers = random.sample(ANSWERS_ELEMENTS, 10)
+        print(answers)
         
+select_theme()
 
 
